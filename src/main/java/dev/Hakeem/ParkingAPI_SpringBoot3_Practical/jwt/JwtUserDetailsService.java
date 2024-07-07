c4467c766ef4017f43726b84e3e5a8b08f3191d1
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-07 17:38:59 -0300

    Criando a classe JwtUserDetailsService

---
+++ src/main/java/dev/Hakeem/ParkingAPI_SpringBoot3_Practical/jwt/JwtUserDetailsService.java
@@ -0,0 +1,30 @@
+package dev.hakeem.parkingapi_springboot3_practical.jwt;
+
+import dev.hakeem.parkingapi_springboot3_practical.entities.Role;
+import dev.hakeem.parkingapi_springboot3_practical.entities.User;
+import dev.hakeem.parkingapi_springboot3_practical.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+       private  final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.uscarPorUsername(username);
+        return  new JwtUserDetails(user);
+    }
+
+
+    public JwtToken getTokenAuthenticated(String username) {
+       Role role = userService.buscarRolePorUseername(username);
+        return JwtUtils.createToken(username,role.name().substring("ROLE_".length()));
+    }
+}
